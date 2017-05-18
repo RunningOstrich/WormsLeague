391195f48a3fbbf42a6bb58a5eb4a1ba1c05c7b9
David Eadie <david.eadie@red-gate.com>
2017-05-18 14:42:16 +0100

    Add some replay extractor code
    I wrote this about a year a go

---
+++ WormsStats/WormsStats.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using WormsStats.ReplayExtractor;
+
+namespace WormsStats.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string wormsPath = @"C:\Program Files (x86)\Steam\steamapps\common\Worms Armageddon\";
+            const string replayPath = @"C:\Program Files (x86)\Steam\steamapps\common\Worms Armageddon\User\Games\";
+
+            var replayExtractor = new ReplayLogExtractor(wormsPath, replayPath);
+            replayExtractor.ExtractAll();
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ WormsStats/WormsStats.ReplayExtractor/ReplayLogExtractor.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace WormsStats.ReplayExtractor
+{
+    public class ReplayLogExtractor
+    {
+        private readonly string m_WormsReplayPath;
+        private readonly string m_WormsExe;
+
+        public ReplayLogExtractor(string wormsPath, string wormsReplayPath)
+        {
+            m_WormsReplayPath = wormsReplayPath;
+            m_WormsExe = Path.Combine(wormsPath, "WA.exe");
+        }
+
+        public void ExtractAll()
+        {
+            var d = new DirectoryInfo(m_WormsReplayPath);
+
+            foreach (var file in d.GetFiles("*.WAgame"))
+            {
+                ExtractReplayFile(file.Name);
+            }
+        }
+
+        public void ExtractReplayFile(string fileName)
+        {
+            var fullPathToReplay = Path.Combine(m_WormsReplayPath, fileName);
+            var parameters = $@"/getLog ""{fullPathToReplay}"" /q";
+
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = m_WormsExe,
+                    Arguments = parameters
+                }
+            };
+
+            proc.Start();
+            proc.WaitForExit();
+        }
+    }
+}
